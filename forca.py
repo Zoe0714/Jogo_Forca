c79ec2533bc895299c0ffddc5130b5f5c8949203
Zoe0714 <159541965+Zoe0714@users.noreply.github.com>
2025-05-06 09:02:39 -0300

    Add files via upload

--- forca.py
+++ forca.py
@@ -3,6 +3,22 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
+    palavra_secreta = "koenigsegg"
+
+    enforcou = False
+    acertou = False
+
+    while (not enforcou and not acertou):
+        chute = input("Qual é a letra?")
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute == letra):
+                print(f'Encontrei a letra {letra} na posição {index}')
+                index = index + 1
+
+        print("Jogando...")
+
     print("Fim do jogo")
 
 if(__name__ == "__main__"):
